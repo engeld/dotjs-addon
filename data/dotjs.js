2070fe857ce7b4f62d4cf4cfe84b3e8d03f7bfb1
kares <self@kares.org>
2013-09-19 18:16:51 +0200

    js stylin

--- data/dotjs.js
+++ data/dotjs.js
@@ -6,19 +6,14 @@
         // bail out if we're in an iframe
         if (window.frameElement) return;
         
-        if (msg.jquery) {
-            eval(msg.jquery);
-        }
+        if (msg.jquery) eval(msg.jquery);
 
-        if (msg.js) {
-            eval(msg.js);
-        }
+        if (msg.js) eval(msg.js);
 
         if (msg.coffee) {
-            (function() {
-                eval(msg.transpiler);
-            }).call(window); // coffee-script.js assumes this === window
-            eval(CoffeeScript.compile(msg.coffee));
+            // coffee-script.js assumes this === window
+            (function() { eval(msg.transpiler); }).call(window);
+            eval( CoffeeScript.compile(msg.coffee) );
         }
 
         if (msg.css) {
@@ -29,7 +24,7 @@
         }
     });
 
-    if (document.URL.indexOf('http') === 0) {
-        self.port.emit('init', document.URL);    
+    if ( document.URL.indexOf('http') === 0 ) {
+        self.port.emit('init', document.URL);
     }
 })();
